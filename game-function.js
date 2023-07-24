5dd7d0f021aa8f325786bd7bd4c2316405db323a
Mel <139094521+Mellove2@users.noreply.github.com>
2023-07-24 14:50:06 -0700

    Add files via upload

---
+++ game-function.js
@@ -0,0 +1,6 @@
+document.write("hey u comin to the party");
+
+setTimeout(() => {prompt("Thoughtbubble: do I really want to go? I mean its 4pm, which i guess it pretty early...for my standards. Yes or No?"); }, 5000)
+
+
+
---
+++ hold.js
@@ -0,0 +1,9 @@
+
+const answer_yes = [yes, y, Yes, Y, YES]
+const answer_no = [no, n, No, N, NO]
+
+Let ans1 = yes
+  if (ans1 = answer_yes){
+    document.write("hi tee hee")
+  }
+    
